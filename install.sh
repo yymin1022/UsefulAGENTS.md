a7bcb2d74db30709badaeeb493ac7423f51b0c98
yymin1022 <yymin1022@gmail.com>
2026-09-25 13:28:57 +0900

    Update install.sh : Added rule file initialization

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo "Hello, World!"
+set -e
+
+CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+RULE_FILE="$CUR_DIR/RULES.md"
+
+if [ ! -f "$RULE_FILE" ]; then
+  echo "No rule file found: $RULE_FILE"
+  exit 1
+fi
